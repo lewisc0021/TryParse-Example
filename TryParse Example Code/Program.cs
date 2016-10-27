eb9a777db7baa63eb0f17cfa0359e9246d9e4cee
Tommy Lewis <lewisc0021@gmail.com>
2016-10-27 13:32:22 -0400

    first commit

---
+++ TryParse Example Code/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParse_Example_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many sides do these die have?");
+            int sides;
+            while (!int.TryParse(Console.ReadLine(), out sides))
+            {
+                Console.WriteLine("Please input a correct value.");
+            }
+
+        }
+    }
+}
